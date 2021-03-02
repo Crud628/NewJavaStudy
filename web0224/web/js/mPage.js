562c31b62e6b9d173331d1d99befc447966e89fa
ff <1109373967@qq.com>
2021-03-02 10:28:33 +0800

    定制BootStrap首页

--- web0224/web/js/mPage.js
+++ web0224/web/js/mPage.js
@@ -63,7 +63,7 @@ function Pages(j) {
         datatype:"json",
         success:function (data) {
             data = $.parseJSON(data);
-            var result="<table border='1' cellspacing='0'><thead><tr><th>员工id</th><th>员工姓名</th><th>员工性别</th><th>员工工资</th><th>员工部门</th><th>员工年龄</th></tr></thead>";
+            var result="<table class='table table-striped table-hover '><thead><tr><th>员工id</th><th>员工姓名</th><th>员工性别</th><th>员工工资</th><th>员工部门</th><th>员工年龄</th></tr></thead>";
             $.each(data,function(i){
                 result +="<tr><td>"+data[i].id
                     +"</td><td>"+data[i].name
